79dfbfc8ef67ed3fb6d4adf02fe330939ffd39c6
jay <kjh338076@naver.com>
2018-06-14 14:49:07 +0900

    Initial Commit

---
+++ interview-job-opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  interview-job-opencv
+//
+//  Created by JunHee on 14/06/2018.
+//  Copyright © 2018 JunHee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
