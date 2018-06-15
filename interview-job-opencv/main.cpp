e70d1f72ad3da5dfce9d1cfcde99e824d30a9b71
Luca Vaquer <lcvaquer@gmail.com>
2018-06-15 19:51:16 +0900

    New

--- interview-job-opencv/main.cpp
+++ interview-job-opencv/main.cpp
@@ -1,132 +1,134 @@
-/**
- * eye-tracking.cpp:
- * Eye detection and tracking with OpenCV
- *
- * This program tries to detect and tracking the user's eye with webcam.
- * At startup, the program performs face detection followed by eye detection
- * using OpenCV's built-in Haar cascade classifier. If the user's eye detected
- * successfully, an eye template is extracted. This template will be used in
- * the subsequent template matching for tracking the eye.
- */
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+
+//  main.cpp
+//  blink_counter
+//
+//  Created by AndyWu on 17/08/2017.
+//  Copyright © 2017 AndyWu. All rights reserved.
+//
+
+#include <iostream>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/tracking/tracker.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/tracking.hpp>
+#include <fstream>
 
-cv::CascadeClassifier face_cascade;
-cv::CascadeClassifier eye_cascade;
+using namespace std;
+using namespace cv;
 
-/**
- * Function to detect human face and the eyes from an image.
- *
- * @param  im    The source image
- * @param  tpl   Will be filled with the eye template, if detection success.
- * @param  rect  Will be filled with the bounding box of the eye
- * @return zero=failed, nonzero=success
- */
-int detectEye(cv::Mat& im, cv::Mat& tpl, cv::Rect& rect)
-{
-    std::vector<cv::Rect> faces, eyes;
-    face_cascade.detectMultiScale(im, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30,30));
+string inputDir = "/usr/local/Cellar/opencv/3.4.1_2/share/OpenCV/haarcascades/";
+String face_cascade_name = inputDir + "haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = inputDir + "haarcascade_lefteye_2splits.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+
+void thresholdAndOpen(Mat src, Mat dst); //threshold and consequent bluring operation
+void thresholdInRange(Mat src, Mat dst); //figure out the range of the eye_blink_diff
+vector<Rect> detectEyeAndFace(Mat src);
+
+int main(){
+    VideoCapture cap;
+    cap.open(0);
     
-    for (int i = 0; i < faces.size(); i++)
-    {
-        cv::Mat face = im(faces[i]);
-        eye_cascade.detectMultiScale(face, eyes, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(20,20));
-        
-        if (eyes.size())
-        {
-            rect = eyes[0] + cv::Point(faces[i].x, faces[i].y);
-            tpl  = im(rect);
-        }
+    if(!cap.isOpened()){
+        cout << "fail to open camera" << endl;
+        return -1;
     }
     
-    return eyes.size();
-}
-
-/**
- * Perform template matching to search the user's eye in the given image.
- *
- * @param   im    The source image
- * @param   tpl   The eye template
- * @param   rect  The eye bounding box, will be updated with the new location of the eye
- */
-void trackEye(cv::Mat& im, cv::Mat& tpl, cv::Rect& rect)
-{
-    cv::Size size(rect.width * 2, rect.height * 2);
-    cv::Rect window(rect + size - cv::Point(size.width/2, size.height/2));
+    Mat frame[2], residue;
+    namedWindow("camera", 1);
+    namedWindow("residue", 1);
+    cap >> frame[1];
+    cvtColor(frame[1], frame[1], COLOR_BGR2GRAY);
+    pyrDown(frame[1], frame[1]);
+    cout << frame[1].size[0] << endl << frame[1].size[1] << endl;
     
-    window &= cv::Rect(0, 0, im.cols, im.rows);
-    
-    cv::Mat dst(window.width - tpl.rows + 1, window.height - tpl.cols + 1, CV_32FC1);
-    cv::matchTemplate(im(window), tpl, dst, CV_TM_SQDIFF_NORMED);
-    
-    double minval, maxval;
-    cv::Point minloc, maxloc;
-    cv::minMaxLoc(dst, &minval, &maxval, &minloc, &maxloc);
-    
-    if (minval <= 0.2)
-    {
-        rect.x = window.x + minloc.x;
-        rect.y = window.y + minloc.y;
+    //  init face and eye casacade
+    if( !face_cascade.load( face_cascade_name ) ){
+        printf("face_cascade_name failed to load...\n");
+        getchar();
     }
-    else
-        rect.x = rect.y = rect.width = rect.height = 0;
-}
-
-int main(int argc, char** argv)
-{
-    // Load the cascade classifiers
-    // Make sure you point the XML files to the right path, or
-    // just copy the files from [OPENCV_DIR]/data/haarcascades directory
-    face_cascade.load("/usr/local/Cellar/opencv/3.4.1_2/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml");
-    eye_cascade.load("/usr/local/Cellar/opencv/3.4.1_2/share/OpenCV/haarcascades/haarcascade_eye.xml");
-    
-    // Open webcam
-    cv::VideoCapture cap(0);
-    
-    // Check if everything is ok
-    if (face_cascade.empty() || eye_cascade.empty() || !cap.isOpened())
-        return 1;
-    
-    // Set video to 320x240
-    cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
-    
-    cv::Mat frame, eye_tpl;
-    cv::Rect eye_bb;
-    
-    while (cv::waitKey(15) != 'q')
-    {
-        cap >> frame;
-        if (frame.empty())
-            break;
-        
-        // Flip the frame horizontally, Windows users might need this
-        cv::flip(frame, frame, 1);
-        
-        // Convert to grayscale and
-        // adjust the image contrast using histogram equalization
-        cv::Mat gray;
-        cv::cvtColor(frame, gray, CV_BGR2GRAY);
-        
-        if (eye_bb.width == 0 && eye_bb.height == 0)
-        {
-            // Detection stage
-            // Try to detect the face and the eye of the user
-            detectEye(gray, eye_tpl, eye_bb);
-        }
-        else
-        {
-            // Tracking stage with template matching
-            trackEye(gray, eye_tpl, eye_bb);
-            
-            // Draw bounding rectangle for the eye
-            cv::rectangle(frame, eye_bb, CV_RGB(0,255,0));
-        }
-        
-        // Display video
-        cv::imshow("video", frame);
+    if( !eyes_cascade.load( eyes_cascade_name ) ){
+        printf("eye_cascade_name failed to load...\n");
+        getchar();
     }
     
-    return 0;
+    // create tracker
+    //    Ptr<Tracker> tracker = Tracker::create();
+    
+    
+    
+    //  main process
+    bool is_tracking = false;
+    Rect box;
+    
+    for(int i=0; waitKey(1) != 27; i=!i){
+        double start = getTickCount(); //to count the period the process takes
+        
+        cap >> frame[i];
+        cvtColor(frame[i], frame[i], COLOR_BGR2GRAY);
+        pyrDown(frame[i], frame[i]);
+        //equalizeHist(frame[i], frame[i]);
+        residue = frame[i] - frame[!i];
+        
+        //thresholdAndOpen(residue, residue);
+        //thresholdInRange(residue, residue);
+        //        if(!is_tracking){
+        vector<Rect> eyes = detectEyeAndFace(frame[i]);
+        //            if(eyes.size()){
+        //                box = eyes[0];
+        //                tracker->init(frame[i], box);
+        //                is_tracking = true;
+        //            }
+        //        }
+        
+        
+        
+        //        rectangle(frame[i], box, 255, 3);
+        for(size_t j=0; j<eyes.size(); j++)
+            rectangle(frame[i], eyes[j], 255, 3);
+        
+        imshow("camera", frame[i]);
+        //imshow("residue", residue);
+        
+        cout << ((getTickCount() - start) / getTickFrequency()) * 1000 << endl; //output the time the process takes
+    }
 }
+
+void thresholdAndOpen(Mat src, Mat dst){
+    threshold(src, dst, 10, 255, THRESH_BINARY);
+    int maskSize = 5;
+    //threshold(residue, residue, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, maskSize, 0);
+    Mat ele = getStructuringElement(MORPH_ELLIPSE, Size(maskSize, maskSize));
+    morphologyEx(src, dst, MORPH_OPEN, ele);
+}
+
+void thresholdInRange(Mat src, Mat dst){
+    inRange(src, 10, 30, dst);
+    int maskSize = 5;
+    Mat ele = getStructuringElement(MORPH_ELLIPSE, Size(maskSize, maskSize));
+    morphologyEx(src, dst, MORPH_OPEN, ele);
+}
+
+vector<Rect> detectEyeAndFace(Mat src){
+    vector<Rect> faces, eyes;
+    
+    face_cascade.detectMultiScale( src, faces, 1.1, 2, 0, Size(30, 30));
+    
+    for( size_t i = 0; i < faces.size(); i++)
+    {
+        Mat faceROI = src( faces[i] );
+        
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0, Size(30, 30));
+        
+        for( size_t j = 0; j < eyes.size(); j++)
+            eyes[j] += faces[i].tl();
+    }
+    
+    return eyes;
+}
+
