b10480ffc4fe8074340cf590e0de59d6f1da84b7
Luca Vaquer <lcvaquer@gmail.com>
2018-06-15 20:44:39 +0900

    Added color camera stream

--- interview-job-opencv/main.cpp
+++ interview-job-opencv/main.cpp
@@ -46,7 +46,7 @@ int main(){
     namedWindow("camera", 1);
     namedWindow("residue", 1);
     cap >> frame[1];
-    cvtColor(frame[1], frame[1], COLOR_BGR2GRAY);
+    cvtColor(frame[1], frame[1], COLOR_BGR2BGRA);
     pyrDown(frame[1], frame[1]);
     cout << frame[1].size[0] << endl << frame[1].size[1] << endl;
     
@@ -73,7 +73,7 @@ int main(){
         double start = getTickCount(); //to count the period the process takes
         
         cap >> frame[i];
-        cvtColor(frame[i], frame[i], COLOR_BGR2GRAY);
+        cvtColor(frame[i], frame[i], COLOR_BGR2BGRA);
         pyrDown(frame[i], frame[i]);
         //equalizeHist(frame[i], frame[i]);
         residue = frame[i] - frame[!i];
