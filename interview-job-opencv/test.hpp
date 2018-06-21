1952bc53b8b0f466c489037638018d8db65d5d24
jay <kjh338076@naver.com>
2018-06-21 15:44:57 +0900

    add head angle code, but doesnt work

---
+++ interview-job-opencv/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  interview-job-opencv
+//
+//  Created by JunHee on 21/06/2018.
+//  Copyright © 2018 JunHee. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
